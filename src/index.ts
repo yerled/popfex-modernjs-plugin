ed730747ac19c7af77a5b9f3d5a42b64163c95a4
袁广龙 <yuanguanglong@popmart.com>
2022-03-30 18:23:51 +0800

    feat: demo

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-import { IPluginContext, ForgedAPI } from '@modern-js/generator-plugin';
+import {
+  IPluginContext,
+  ForgedAPI,
+  FileType,
+} from '@modern-js/generator-plugin';
 
 export default function (context: IPluginContext) {
   context.onForged(async (_api: ForgedAPI, _input: Record<string, unknown>) => {
     /**
      * todo
      */
+  });
+
+  context.onForged(async (api: ForgedAPI) => {
+    api.addFile({
+      type: FileType.Text,
+      file: `src/haha/index.ts`,
+      templateFile: `haha.ts.handlebars`,
+      force: true,
+    });
+
+    api.addFile({
+      type: FileType.Text,
+      file: `src/xixi.ts`,
+      templateFile: `haha.ts.handlebars`,
+      force: true,
+    });
   });
 }
